508b56d038edd4485ab3df93f93c82ae3a8fd44e
omerdemirarslan <omer.demirarslan@bonintel.com>
2022-08-11 17:00:28 +0300

    FTA-02, Create Models With First Migration and Update Some Files.

--- flight_tracking/models.py
+++ flight_tracking/models.py
@@ -9,18 +9,12 @@ class Airport(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        db_table = 'airports_information'
-
 
 class Flight(models.Model):
     flight_number = models.CharField(max_length=25, db_index=True)
     take_off = models.DateTimeField(null=False)
     landing = models.DateTimeField(null=False)
-    location = models.ForeignKey(to=Airport, related_name='flights_location', on_delete=models.CASCADE)
     destination = models.ForeignKey(to=Airport, related_name='flights_destination', on_delete=models.CASCADE)
+    location = models.ForeignKey(to=Airport, related_name='flights_location', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = 'flights_information'
